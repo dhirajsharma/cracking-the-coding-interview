ae792a7094146012b51e8d8e42960e809faa30ff
Allan Reyes <allanbreyes@gmail.com>
2016-11-21 21:12:56 -0800

    Add 1.1

--- src/arrays_and_strings.py
+++ src/arrays_and_strings.py
@@ -1,1 +1,38 @@
 CHAPTER = 1
+
+
+# 1.1
+# Is Unique:
+# - Implement an alogirthm to determine if a string has all unique characters.
+# - What if you cannot use additional data structures?
+def is_unique(string):
+    # O(1) trivial case
+    if len(string) > 128:
+        return False
+
+    # O(n) implementation
+    return is_unique_with_hash(string)
+
+    # O(n^2) time, O(1) space
+    # return is_unique_without_hash(string)
+
+def is_unique_with_hash(string):
+    seen = {}
+    for char in string:
+        if char in seen:
+            return False
+        else:
+            seen[char] = True
+    return True
+
+def is_unique_without_hash(string):
+    # O(n*log(n)) time (sorted)
+    ordered = sorted(string)        # O(log(n))
+    for i in range(len(ordered)):   # O(n)
+        if i >= len(ordered) - 1:
+            continue
+
+        if ordered[i] == ordered[i+1]:
+            return False
+
+    return True
--- test/arrays_and_strings_test.py
+++ test/arrays_and_strings_test.py
@@ -3,12 +3,22 @@ import unittest
 
 
 class ArraysAndStringsTest(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def testChapter(self):
+    def test_chapter(self):
         self.assertEqual(subject.CHAPTER, 1)
+
+
+class IsUnique(unittest.TestCase):
+    def test_trivial(self):
+        self.assertEqual(subject.is_unique(''), True)
+
+    def test_simple(self):
+        self.assertEqual(subject.is_unique('abcdef'), True)
+
+    def test_repeated(self):
+        self.assertEqual(subject.is_unique('ababababa'), False)
+
+    def test_worst_case(self):
+        self.assertEqual(subject.is_unique('abcdefghijklmnopp'), False)
 
 if __name__ == '__main__':
     unittest.main()
