f8236f1fca38555e46559fa6ee2537f2699778cb
allanbreyes <allanbreyes@gmail.com>
2016-11-28 21:10:04 -0800

    Add boilerplate code for linked lists problems

--- src/linked_lists.py
+++ src/linked_lists.py
@@ -12,10 +12,12 @@ class First:
     pass
 
 class LinkedList:
-    def __init__(self):
+    def __init__(self, *values):
         self.first = None
         self.last = None
-        self.nodes = []
+
+        for value in values:
+            self.append(value)
 
     def __str__(self):
         if self.first is not None:
@@ -48,5 +50,57 @@ class LinkedList:
         else:
             return [start] + self.list(start.next)
 
+    def values(self):
+        return [node.value for node in self.list()]
+
     def clear(self):
         self.__init__()
+
+# 2.1 Remove Dups: Write code to remove duplicates from an unsorted linked list.
+# How would you solve this problem if a temporary buffer is not allowed?
+def remove_duplicates(linked_list):
+    return linked_list
+
+# 2.2 Return Kth to Last: Implement an algorithm to find the kth to last element
+# of a singly linked list.
+def last(linked_list, k):
+    return linked_list.last
+
+# 2.3 Delete Middle Node: Implement an algorithm to delete a node in the middle
+# (i.e. any node but the first and last node, not necessarily the exact middle)
+# of a singly linked list, given only access to that node.
+def delete_node(linked_list, node):
+    return linked_list
+
+# 2.4 Partition: Write code to partition a linked list around a value x, such
+# that all nodes less than x come before all nodes greater than or equal to x.
+# If x is contained within the list, the values of x only need to be after the
+# elements less than x. The partition element x can appear anywhere in the
+# "right partition"; it does not need to appear between the left and right
+# partitions.
+def partition(linked_list, x):
+    return linked_list
+
+# 2.5 Sum Lists: You have two numbers represented by a linked list, where each
+# node contains a single digit. The digits are stored in reverse order such that
+# the 1's digit is at the head of the list. Write a function that adds the two
+# numbers and returns the sum as a linked list.
+def sum_lists(l1, l2):
+    return l1 + l2
+
+# 2.6 Palindrome: Create a function to check if a linked list is a palindrome.
+def is_palindrome(linked_list):
+    return False
+
+# 2.7 Intersection: Given two (singly) linked lists, determine if the two lists
+# intersect. Return the intersecting node. Note that the intersection is defined
+# based on reference, not value. That is, if the kth node of the first linked
+# list is the exact same node (by reference) as the jth node of the second
+# linked list, then they are intersecting.
+def intersection(l1, l2):
+    pass
+
+# 2.8 Loop Detection: Given a circular linked list, implement an algorithm that
+# returns the node at the beginning of the loop.
+def detect_loop(linked_list):
+    pass
--- test/linked_lists_test.py
+++ test/linked_lists_test.py
@@ -6,11 +6,38 @@ class LinkedListsTest(unittest.TestCase):
         self.assertEqual(subject.CHAPTER, 2)
 
     def test_linked_list(self):
-        ll = subject.LinkedList()
-        ll.append('alpha')
-        ll.append('bravo')
-        ll.append('charlie')
-        self.assertEqual([node.value for node in ll.list()], ['alpha', 'bravo', 'charlie'])
+        ll = subject.LinkedList('alpha', 'bravo', 'charlie')
+        self.assertEqual(ll.values(), ['alpha', 'bravo', 'charlie'])
+
+class RemoveDuplicatesTest(unittest.TestCase):
+    def test_unique(self):
+        ll = subject.LinkedList('alpha', 'bravo', 'charlie')
+        self.assertEqual(subject.remove_duplicates(ll).list(), ll.list())
+
+    def test_duplicates(self):
+        ll = subject.LinkedList('alpha', 'alpha', 'bravo')
+        self.assertEqual(subject.remove_duplicates(ll).values(), ['alpha', 'bravo'])
+
+class ReturnKthToLastTest(unittest.TestCase):
+    pass
+
+class DeleteMiddleNodeTest(unittest.TestCase):
+    pass
+
+class PartitionTest(unittest.TestCase):
+    pass
+
+class SumListsTest(unittest.TestCase):
+    pass
+
+class PalindromeTest(unittest.TestCase):
+    pass
+
+class IntersectionTest(unittest.TestCase):
+    pass
+
+class LoopDetectionTest(unittest.TestCase):
+    pass
 
 if __name__ == '__main__':
     unittest.main()
